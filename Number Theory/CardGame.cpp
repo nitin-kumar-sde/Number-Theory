91b4393772cfec8e3470c1008377e06ed177127c
Nitin Kumar <nitin.kumar.sde@gmail.com>
2025-07-26 16:01:29 +0530

    Update CardGame.cpp

--- Number Theory/CardGame.cpp
+++ Number Theory/CardGame.cpp
@@ -3,76 +3,89 @@
 #include <string>
 #include <set>
 #include <map>
-#include <unordered_map>
-#include <algorithm>
-#include <cmath>
-#include <fstream>
 using namespace std;
 
-#define ll long long
-#define ld long double
-#define endl '\n'
-#define N 1000005
-#define MOD 1000000007
+using ll = long long;
+using pll = pair<ll, ll>;
 
-ll getFirstValidEndIndex(ll * prefixProduct, ll start, ll n, ll k) {
-
-    ll ans = n; ll low = start; ll high = n-1;
-
-    while(low <= high) {
-        ll mid = (low + high)/2;
-
-        ll targetProduct = prefixProduct[mid];
-        
-        if(prefixProduct[start-1] == 0) 
-        return n-1;
-        if(start !=0) {
-            targetProduct/= prefixProduct[start-1];
+// prime factorise
+map<ll, int> get_prime_factors(ll k) {
+    map<ll, int> factors;
+    for (ll i = 2; i * i <= k; ++i) {
+        while (k % i == 0) {
+            ++factors[i];
+            k /= i;
         }
-
-        if(targetProduct%k == 0) {
-            ans = mid;
-            high = mid-1;
-        }
-        else low = mid + 1;
-        
     }
-    return ans;
-
+    if (k > 1) factors[k]++;
+    return factors;
 }
+
+// counts power of prime in a given num
+int count_prime_power(int x, int p) {
+    int cnt = 0;
+    while (x % p == 0) {
+        ++cnt;
+        x /= p;
+    }
+    return cnt;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-    #ifndef ONLINE_JUDGE
-        freopen("/Users/nitinkumar/Desktop/DSA_CPP/input.txt", "r", stdin);
-        freopen("/Users/nitinkumar/Desktop/DSA_CPP/output.txt", "w", stdout);
-    #endif
+    int n;
+    ll k;
+    cin >> n >> k;
 
-    ll n, k; cin >> n >> k;
+    vector<int> a(n+1,0);
+    for (int i=1;i<=n;i++) cin >> a[i];
 
-    ll * input = new ll [n]; 
+    map<ll, int> k_factors = get_prime_factors(k);
+    int primes_count = k_factors.size();
 
-    for(int i=0;i<n;i++) cin >> input[i];
+    vector<vector<int>> prefix(primes_count, vector<int>(n + 1, 0));
+    vector<ll> primes;
 
-    ll * prefixProduct = new ll [n];
+    for (auto &[p, e] : k_factors) primes.push_back(p);
 
-    prefixProduct[0] = input[0];
-    for(int i=1; i<n; i++) 
-    prefixProduct[i] = prefixProduct[i-1] * input[i];
-
-    ll count = 0;
-    
-    for(ll i=0;i<n;i++) {
-
-        ll index = getFirstValidEndIndex(prefixProduct, i, n, k);
-        if(index == n) continue;
-        count = count + (n-index);
-        // cout << i << " " << prefixProduct[i]<< " " << index << endl;
+    for (int i = 1; i <=n; ++i) {
+        int idx = 0;
+        for (auto p : primes) {
+            int cnt = count_prime_power(a[i], p);
+            prefix[idx][i] = prefix[idx][i-1] + cnt;
+            ++idx;
+        }
     }
 
-    cout << count << endl;
+    ll total = 0;
+    for (int l = 1; l <=n; ++l) {
+        int low = l, high = n, r = n+1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            bool valid = true;
+            int idx = 0;
+            for (auto &[p, e] : k_factors) {
 
+                int cnt = prefix[idx][mid] - prefix[idx][l-1];
+
+                if (cnt < e) {
+                    valid = false;
+                    break;
+                }
+                ++idx;
+            }
+            if (valid) {
+                r = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        total += n + 1  - r;
+    }
+
+    cout << total << endl;
     return 0;
 }
