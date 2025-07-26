5be3973ca9ce88ce4db9e557093d27920e80e9b4
Nitin Kumar <nitin.kumar.sde@gmail.com>
2025-07-26 16:45:45 +0530

    Update StrangeNumbers.cpp

--- Number Theory/StrangeNumbers.cpp
+++ Number Theory/StrangeNumbers.cpp
@@ -12,11 +12,13 @@ using namespace std;
 #define ll long long
 #define ld long double
 #define endl '\n'
-#define N 5000005
+#define N 2000005
 #define MOD 1000000007
 
 vector<int> lowestPrimeDivisor(N+1, 1);
+int marked [N]= {0};
 
+// builds lowest prime divisor of a number
 void buildSieve() {
     
     for(int i=2; i<=N; i++) {
@@ -29,41 +31,48 @@ void buildSieve() {
     }
 
 }
+
+// prime factorisation based on the lpd
+vector<int> getPrimeDivisor(int num) {
+    vector<int> primes;
+    int x = lowestPrimeDivisor[num];
+
+    while(x>1) {
+        primes.push_back(x);
+        while(num%x == 0) num/=x;
+        x= lowestPrimeDivisor[num];
+    }
+    return primes;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    #ifndef ONLINE_JUDGE
-        freopen("/Users/nitinkumar/Desktop/DSA_CPP/input.txt", "r", stdin);
-        freopen("/Users/nitinkumar/Desktop/DSA_CPP/output.txt", "w", stdout);
-    #endif
-
     buildSieve();
 
     ll n; cin >> n;
 
-    ll * marked = new ll [n+1];
+    
     for(int i=1;i<=n;i++) marked[i] = false;
-    vector<ll> strangeOrder;
     vector<ll> strangeOrderFinal;
 
     for(int i=n;i>0;i--) {
         if(marked[i]) continue;
-        strangeOrder = {};
+        vector<ll> strangeOrder;
         int currentElemenet = i;
-        int x = lowestPrimeDivisor[currentElemenet];
-        while(x>1) {
-            int current = i;
-            while(current >=1) {
-                if(!marked[current])
-                strangeOrder.push_back(current);
-                marked[current] = true;
-                current-=x;
+        vector<int> divisors = getPrimeDivisor(currentElemenet);
+
+        for(int pd : divisors) {
+            currentElemenet = i;
+            while(currentElemenet >=1) {
+                if(!marked[currentElemenet]) {
+                strangeOrder.push_back(currentElemenet);
+                marked[currentElemenet] = true;
+                }
+                currentElemenet-=pd;
             }
-            while(currentElemenet%x == 0) 
-            currentElemenet/=x;
-            x= lowestPrimeDivisor[currentElemenet];
         }
        
         sort(strangeOrder.begin(), strangeOrder.end(), greater<int>());
@@ -73,8 +82,6 @@ int main() {
     
 
     strangeOrderFinal.push_back(1);
-    // auto last = unique(strangeOrderFinal.begin(), strangeOrderFinal.end());
-    // strangeOrderFinal.erase(last, strangeOrderFinal.end());
 
     for(int i=0;i<strangeOrderFinal.size();i++) 
     cout << strangeOrderFinal[i] << " ";
